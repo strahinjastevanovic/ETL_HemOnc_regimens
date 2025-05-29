551fa72202b6bdf9d4b569e74325f16cbd091029
Svetozar Nesic <snesic@gmail.com>
2025-05-29 17:19:19 +0200

    add condition to sigs

--- queries/vocab_query.py.R
+++
@@ -1,9 +0,0 @@
-# Doc: Description: "athena vocab queries" - pkg requirments - if any 
-
-# backend 
-
-# mapping:
-# input: INPUT_FILES_HEMONC/sigs.csv full-join regimen_cui -> condition_cui many to many
-
-# output: sigs_conditions.csv ( optional: total number sumstats )
-
